3ebf32940a14203d12ace3b772740203af8178ff
Dylan Fennelly <D00248176@student.dkit.ie>
2022-11-26 15:50:55 +0000

    Intital Commit

---
+++ src/main/java/FarmSimulator/App.java
@@ -0,0 +1,13 @@
+package FarmSimulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
